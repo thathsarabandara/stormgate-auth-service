a295636fd78e488119cbe44f2e1547f6c798f73d
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-16 09:18:04 +0530

    add dtos verify otp request

--- src/main/java/com/thathsara/authservice/auth_service/dto/AuthResponse.java
+++ src/main/java/com/thathsara/authservice/auth_service/dto/AuthResponse.java
@@ -1,5 +1,12 @@
 package com.thathsara.authservice.auth_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
-    
+    private String accessToken;
+    private String refreshToken;
+    private String message;
 }
