8e2518f3803f11a9562851f384915dd343fe947d
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-16 09:46:23 +0530

    add user repository user password repository and refreshtoken repository

--- src/main/java/com/thathsara/authservice/auth_service/repository/UserRepository.java
+++ src/main/java/com/thathsara/authservice/auth_service/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.thathsara.authservice.auth_service.repository;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thathsara.authservice.auth_service.model.User;
+
+public interface  UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
+    boolean existByEmail(String email);
 }
