c7ca381be8b47aab1b92c376b26ca92ff98cd0b2
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-17 13:43:55 +0530

    add verify my services

--- src/main/java/com/thathsara/authservice/auth_service/controller/AuthController.java
+++ src/main/java/com/thathsara/authservice/auth_service/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.thathsara.authservice.auth_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
